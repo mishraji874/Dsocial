3204feb64d2c3f517778da9643c0672d537c923e
Aditya <adityam874@gmail.com>
2023-10-29 08:07:30 +0530

    first commit

---
+++ src/component/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer className="bg-dark text-center p-3 text-white">
+                <h3>Developed from Team Delta by Aditya Mishra</h3>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
